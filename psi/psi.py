ff8793c12a8e6a2152c6b84c7cbb65df9998e7bc
piotr.kawa <piotr.w.kawa@gmail.com>
2019-05-21 23:20:35 +0200

    PSI implemented

---
+++ psi/psi.py
@@ -0,0 +1,98 @@
+import random
+import pdb
+
+from charm.schemes.pkenc.pkenc_rsa import RSA
+from charm.toolbox.integergroup import IntegerGroupQ, integer
+
+SECURITY_PARAM = 20
+X_SIZE = 10
+Y_SIZE = 10
+
+G = IntegerGroupQ()
+G.paramgen(SECURITY_PARAM)
+N = 127
+g = G.random()
+
+
+def generate_Gs(X):
+
+    # hash each element of the set
+    gs = [G.hash(x) for x in X]
+
+    # pick a random a
+    a = G.random()
+
+    # generate big G = g ** a
+    Gs = [g ** a for g in gs]
+
+    return Gs, a
+
+
+def generate_Hs_and_Bs(Y, Gs):
+
+    # hash each element of the set
+    hs = [G.hash(x) for x in Y]
+
+    # pick a random b
+    b = G.random()
+
+    # generate big H = h ** b
+    Hs = [h ** b for h in hs]
+
+    # generate Bs
+    Bs = [G ** b for G in Gs]
+
+    return Hs, Bs
+
+
+def generate_As(Hs, a):
+    As = [H ** a for H in Hs]
+    return As
+
+
+def generate_sets(number_of_mutual):
+    mutual_elements = generate_elements(number_of_mutual)
+
+    print(mutual_elements)
+    X = generate_elements(X_SIZE)
+    Y = [e for e in generate_elements(Y_SIZE) if e not in X]
+
+    X += mutual_elements
+    Y += mutual_elements
+
+    # shuffle sets
+    # random.shuffle(X)
+    # random.shuffle(Y)
+
+    return X, Y
+
+
+def generate_elements(number_of_elements):
+    return [g ** G.random() for _ in range(number_of_elements)]
+
+
+def main():
+
+    number_of_mutual = 2
+
+    # generate sets
+    X, Y = generate_sets(2)
+
+    # generate Gs
+    Gs, a = generate_Gs(X)
+
+    # generate Hs and Bs
+    Hs, Bs = generate_Hs_and_Bs(Y, Gs)
+
+    # generate As
+    As = generate_As(Hs, a)
+    # print(X, Y)
+    a_mutual = [el1 for el1, el2 in zip(X, Bs) if el2 in As]
+    print(a_mutual)
+    # pdb.set_trace()
+
+    # B side compares
+
+
+if __name__ == "__main__":
+    main()
